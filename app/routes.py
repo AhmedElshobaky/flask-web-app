20455883a20fd0aae1bbeea99f322bd21fb0f2a2
Ahmed Aly <ahmedelshobki@gmail.com>
2022-10-30 06:43:18 +0200

    chapter 2 done

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
-
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello world!"
+    user = {'username': 'Ahmed'}
+    posts = [
+        {
+            'auther' : {'username': 'Another User'},
+            'body' : 'What a beautiful day!'
+        },
+        {
+            'auther' : {'username': 'Not ahmed'},
+            'body': 'WOW, This post is posting'
+        }
+
+    ]
+    return render_template('index.html', title = 'Home', user = user, posts = posts)
