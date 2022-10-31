003f734e6f6e4a7d4906814ef1d61482f7d09f33
Ahmed Aly <ahmedelshobki@gmail.com>
2022-10-31 10:32:14 +0200

    Avater to user using gravatar with identicon

--- app/models.py
+++ app/models.py
@@ -3,15 +3,20 @@ from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import login
+from hashlib import md5
 
 class User(UserMixin,db.Model):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(64), index = True, unique = True)
     email = db.Column(db.String(120), index = True, unique = True)
     password_hash = db.Column(db.String(128))
+
+    #extra information about the user
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
     
     
-    posts = db.relationship('Post', backref='auther', lazy='dynamic')
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
     
     # repr function shows how objects of this class are printed
     def __repr__(self):
@@ -22,6 +27,11 @@ class User(UserMixin,db.Model):
         
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+    
+    #add avatar to user
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest,size)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key =True)
